4265cb35c920dab9d75edd167ab7c1051c528a65
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-01-22 15:04:43 +0900

    seventeenth commit 🚀

--- [Section 2] JavaScript Fundamentals - Part 2/coding challenges.js
+++ [Section 2] JavaScript Fundamentals - Part 2/coding challenges.js
@@ -58,7 +58,6 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
 console.log(bills, tips, totals);
-*/
 
 // <Coding Challenge #3>
 const mark = {
@@ -96,10 +95,42 @@ if (mark.bmi > john.bmi) {
 } else if (john.bmi > mark.bmi) {
     console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
 }
+*/
 
+// <Coding Challenge #4>
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2; // turnary operator
+}
 
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
 
+// for (let i = 0; i < bills.length; i++) {
+//     tips.push(calcTip(bills[i]));
+//     totals.push(bills[i] + tips[i]);
+// }
 
+// The Answer
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+// BONUS
+const calcAverage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        // sum = sum + arr[i];
+        sum += arr[i];
+    }
+    return sum / arr.length;
+}
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
 
 
 
