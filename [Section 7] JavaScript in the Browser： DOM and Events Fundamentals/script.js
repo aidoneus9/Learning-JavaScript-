b900b497bb9c1ee9029437eecd6012dc117fbfb0
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-01-29 23:56:51 +0900

    second commit 🚀

--- [Section 7] JavaScript in the Browser： DOM and Events Fundamentals/script.js
+++ [Section 7] JavaScript in the Browser： DOM and Events Fundamentals/script.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/*
 // <70. PROJECT #1: Guess My Number!>
 console.log(document.querySelector('.message').textContent); // getting the text content from the selected element
 
@@ -15,9 +14,10 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 // with an input field, to get the actual value, use the value property
-*/
 
 // <73. Handling Click Events>
 document.querySelector('.check').addEventListener('click', function () {
   console.log(document.querySelector('.guess').value);
+
+  document.querySelector('.message').textContent = '🎉 Correct Number!';
 });
