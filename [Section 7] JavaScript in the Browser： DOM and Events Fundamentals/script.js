da1f22136da67b2da353837af7f62c99fe12dff5
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-01-30 12:00:20 +0900

    third commit 🚀

--- [Section 7] JavaScript in the Browser： DOM and Events Fundamentals/script.js
+++ [Section 7] JavaScript in the Browser： DOM and Events Fundamentals/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 // <70. PROJECT #1: Guess My Number!>
 console.log(document.querySelector('.message').textContent); // getting the text content from the selected element
 
@@ -14,10 +15,21 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 // with an input field, to get the actual value, use the value property
+*/
 
 // <73. Handling Click Events>
-document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
 
-  document.querySelector('.message').textContent = '🎉 Correct Number!';
-});
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess); // usually whenever we get something from the user interface, for example, from an input field, it usually always is a string
+
+  // usually, the first scenario is always to assume that there is actually no input, and then we need to respond to that somehow
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+}); // if there is no guess, zero is falsy, so guess here is false now, but then we use the NOT operator to convert it true, we can then make this block of code execute(it only executes whenever this whole condition here is true)
