1fbce4b07528c29c8c56f91cca223ada0fbf2b43
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-02-03 15:50:38 +0900

    second commit 🚀

--- [Section 7] PROJECT #3： Pig Game/script.js
+++ [Section 7] PROJECT #3： Pig Game/script.js
@@ -7,7 +7,13 @@ const score0El = document.querySelector('#score--0');
 // hash is the selector for the ID, and a dot is only for classes
 const score1El = document.getElementById('score--1');
 // getElementById is supposed to be a little bit faster than querySelector(guess: only relevant if you're selecting like thousands of elements at once)
-const diceEl = document.querySelector('.dice');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice'); // diceEl for the element
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 // Starting conditions
 score0El.textContent = 0;
@@ -15,4 +21,28 @@ score1El.textContent = 0;
 // here we are specifying numbers, not strings, but JavaScript will then automatically convert them to strings to actually display them on the page
 diceEl.classList.add('hidden');
 
+let currentScore = 0; // this cannot be inside of the function below because then it would be set to zero each time that we click the button, and so therefore it needs to be outside
+
 // <83. Rolling the Dice>
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // dice for the number itself, and this variable needs to be not a global variable, so not a variable outside because each time that we roll the dice, we want to generate a new number
+  // Math.random(): 0 ~ 0.9999 -> truncate(remove the decimal parts): 0 ~ 5
+  console.log(dice);
+
+  // 2. Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  // 3. Check for rolled 1: if true, switch to next player
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    current0El.textContent = currentScore; // CHANGE LATER
+  } else {
+    // Switch to next player
+  }
+});
