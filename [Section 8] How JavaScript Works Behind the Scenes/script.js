73fa08bd4a31d941fdf3e127b86939ba9c0d2417
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-02-12 21:17:46 +0900

    sixth commit 🚀

--- [Section 8] How JavaScript Works Behind the Scenes/script.js
+++ [Section 8] How JavaScript Works Behind the Scenes/script.js
@@ -124,3 +124,12 @@ const calcAgeArrow = birthYear => {
   console.log(this);
 };
 calcAgeArrow(1980);
+
+const jacqueline = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+jacqueline.calcAge(1997);
