b8f8a156c85137783f14ec32ce5431fd8a5ba154
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-04-04 23:34:05 +0900

    first commit 🚀

---
+++ [Section 10] A Closer Look at Functions/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+// <129. Default Parameters>
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
+// this is how we basically skip a default parameter that we want to leave at its default.
+
+// <130. How Passing Arguments Works: Value vs. Reference>
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+};
+
+checkIn(flight, jonas);
