2222818768a2e484f9f814321a5f642984d0aca6
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-04-07 21:32:55 +0900

    fourth commit 🚀

--- [Section 10] A Closer Look at Functions/script.js
+++ [Section 10] A Closer Look at Functions/script.js
@@ -80,3 +80,13 @@ const upperFirstWord = function (str) {
   const [first, ...others] = str.split(' ');
   return [first.toUpperCase(), ...others].join(' ');
 };
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
