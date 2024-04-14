48a26af9d211f8af8fd6c38f1d153366be119893
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-04-14 23:51:38 +0900

    thirteenth commit 🚀

--- [Section 10] A Closer Look at Functions/coding challenges.js
+++ [Section 10] A Closer Look at Functions/coding challenges.js
@@ -38,10 +38,37 @@ const poll = {
   // This generates [0, 0, 0, 0]. More in the next section 😃
   answers: new Array(4).fill(0),
   registerNewAnswer() {
-    prompt(
-      `What is your favorite programming language? \n 0: JavaScript \n 1: Python \n 2: Rust \n 3: C++ \n (Write option number)`
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
     );
+    console.log(answer);
+
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      // Poll results are 13, 2, 4, 1
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
+// poll.registerNewAnswer();
 
-poll.registerNewAnswer();
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call(poll);
+// [5, 2, 3]
+// [1, 5, 3, 9, 6, 1]
