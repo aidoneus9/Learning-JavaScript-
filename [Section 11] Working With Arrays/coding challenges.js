96beb233e1ccd5cf085a878de9a723197a857c3f
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-05-12 18:30:18 +0900

    25th commit 🚀

--- [Section 11] Working With Arrays/coding challenges.js
+++ [Section 11] Working With Arrays/coding challenges.js
@@ -133,3 +133,16 @@ const dogs = [
 
 GOOD LUCK 😀
 */
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+// 1.
+dogs.forEach(dog => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28))); // ✂️ Math.trunc: cut the decimal parts
+console.log(dogs);
+
+// 2.
