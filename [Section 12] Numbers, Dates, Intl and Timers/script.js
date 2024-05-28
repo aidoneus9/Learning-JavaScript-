04a1e030a18995413ab0906e15af0a4bf7225764
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-05-28 16:17:28 +0900

    2nd commit 🚀

--- [Section 12] Numbers, Dates, Intl and Timers/script.js
+++ [Section 12] Numbers, Dates, Intl and Timers/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -305,3 +305,60 @@ console.log(Number.isInteger(23.0)); // true
 console.log(Number.isInteger(23 / 0)); // false
 
 // <172. Math and Rounding>
+// 📌 sqrt: square root
+console.log(Math.sqrt(25)); // 5
+console.log(25 ** (1 / 2)); // 5
+console.log(8 ** (1 / 3)); // 2
+
+// 📌 max & min
+console.log(Math.max(5, 18, 23, 11, 2)); // 23
+console.log(Math.max(5, 18, '23', 11, 2)); // 23
+// -> max function does type coercion
+// -> However, it does not parsing
+console.log(Math.max(5, 18, '23px', 11, 2)); // NaN
+
+console.log(Math.min(5, 18, 23, 11, 2)); // 2
+
+console.log(Math.PI); // 3.141592653589793
+console.log(Math.PI * Number.parseFloat('10px') ** 2); // 314.1592653589793
+
+// 📌
+console.log(Math.trunc(Math.random() * 6) + 1);
+// random: 0 ~ 1
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+// 0...1 -> 0...(max - min) -> min...(max - min + min) -> min...max
+console.log(randomInt(10, 20));
+
+// 📌 Rounding Integers
+console.log(Math.trunc(23.3)); // 23
+
+// -> round to the nearest integer
+console.log(Math.round(23.3)); // 23
+console.log(Math.round(23.9)); // 24
+
+// -> round up
+console.log(Math.ceil(23.3)); // 24
+console.log(Math.ceil(23.9)); // 24
+
+// -> round down
+console.log(Math.floor(23.3)); // 23
+console.log(Math.floor(23.9)); // 23
+
+// -> type coercion
+console.log(Math.floor('23.9')); // 23
+
+// `✍️ floor & trunc: negative number
+console.log(Math.trunc(-23.9)); // -23
+console.log(Math.floor(-23.9)); // -24
+// -> floor is a little bit better than trunc; it works in all situations, no matter if we're dealing with positive or negative numbers.
+
+// 📌 Rounding decimals
+console.log((2.7).toFixed(0)); // 3 (string)
+// -> toFixed return a string and not a number
+console.log((2.7).toFixed(3)); // 2.700 (string)
+console.log((2.345).toFixed(2)); // 2.35 (string)
+console.log(+(2.345).toFixed(0)); // 2 (number)
+
+// ✍️ works in a similar way to the string methods. So this is a number(358), so it's a primitive, and primitives actually don't have methods, and so behind the scenes, JavaScript will do boxing(Boxing: transform this to a number object, then call the method on that object. And then once the operation is finished, it will convert it back to a primitive).
