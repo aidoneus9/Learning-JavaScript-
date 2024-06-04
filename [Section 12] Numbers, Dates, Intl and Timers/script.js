c683b038d47e6925ce3446df56d331ffd1dc9711
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-06-04 19:49:50 +0900

    eighth commit 🚀

--- [Section 12] Numbers, Dates, Intl and Timers/script.js
+++ [Section 12] Numbers, Dates, Intl and Timers/script.js
@@ -81,7 +81,7 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
-const formatMovementDate = function (date) {
+const formatMovementDate = function (date, locale) {
   const calcDaysPassed = (date1, date2) =>
     Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
 
@@ -92,10 +92,18 @@ const formatMovementDate = function (date) {
   if (daysPassed === 1) return 'Yesterday';
   if (daysPassed <= 7) return `${daysPassed} days ago`;
 
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
-  return `${day}/${month}/${year}`;
+  // const day = `${date.getDate()}`.padStart(2, 0);
+  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  // const year = date.getFullYear();
+  // return `${day}/${month}/${year}`;
+  return new Intl.DateTimeFormat(locale).format(date);
+};
+
+const formatCur = function (value, locale, currency) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
 };
 
 const displayMovements = function (acc, sort = false) {
@@ -111,7 +119,9 @@ const displayMovements = function (acc, sort = false) {
     const date = new Date(acc.movementsDates[i]);
     // ✍️ looping over two arrays at the same time; we called forEach method on one of them(92, movs), and then we use the current index to also get the data from some other array; that's gonna be at the same position because we're using the same index
     // ✍️ we can use that string to create a new date object and we need that object, so that then from there, we can call our usual methods to get the date and the month and the year; that's the reason why we need to convert these strings back into a JavaScript object, because only then, we can actually work with that data
-    const displayDate = formatMovementDate(date);
+    const displayDate = formatMovementDate(date, acc.locale);
+
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
 
     const html = `
       <div class="movements__row">
@@ -119,7 +129,7 @@ const displayMovements = function (acc, sort = false) {
       i + 1
     } ${type}</div>
         <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${mov.toFixed(2)}€</div>
+        <div class="movements__value">${formattedMov}</div>
       </div>
     `;
 
@@ -129,19 +139,19 @@ const displayMovements = function (acc, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -151,7 +161,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
 };
 
 const createUsernames = function (accs) {
@@ -186,6 +196,30 @@ updateUI(currentAccount);
 containerApp.style.opacity = 100;
 
 /*
+// Experimenting API
+const now = new Date();
+const options = {
+  hour: 'numeric',
+  minute: 'numeric',
+  day: 'numeric',
+  // month: 'numeric',
+  month: 'long',
+  // month: '2-digit',
+  year: 'numeric',
+  // year: '2-digit',
+  weekday: 'long',
+  // weekday: 'short',
+  // weekday: 'narrow',
+};
+const locale = navigator.language;
+console.log(locale); // ko-KR
+
+labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(now);
+// ✍️ locale string: language-country
+// -> 📎 Google ISO language code table: Lingoes
+*/
+
+/*
 const now = new Date();
 const day = `${now.getDate()}`.padStart(2, 0); // As of 01/06/2024, 18:31
 const month = `${now.getMonth() + 1}`.padStart(2, 0); // ∵ zero-based // As of 01/06/2024, 18:31
@@ -216,12 +250,35 @@ btnLogin.addEventListener('click', function (e) {
 
     // Create current date and time
     const now = new Date();
-    const day = `${now.getDate()}`.padStart(2, 0);
-    const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    const year = now.getFullYear();
-    const hour = `${now.getHours()}`.padStart(2, 0);
-    const min = `${now.getMinutes()}`.padStart(2, 0);
-    labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric',
+      // month: 'long',
+      // month: '2-digit',
+      year: 'numeric',
+      // year: '2-digit',
+      // weekday: 'long',
+      // weekday: 'short',
+      // weekday: 'narrow',
+    };
+    // locale from the browser 💻
+    // const locale = navigator.language;
+    // console.log(locale); // ko-KR
+
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now);
+    // ✍️ locale string: language-country
+    // -> 📎 Google ISO language code table: Lingoes
+    // const day = `${now.getDate()}`.padStart(2, 0);
+    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
+    // const year = now.getFullYear();
+    // const hour = `${now.getHours()}`.padStart(2, 0);
+    // const min = `${now.getMinutes()}`.padStart(2, 0);
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -598,3 +655,30 @@ console.log(days1);
 
 const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
 console.log(days1);
+
+// <179. Internationalizing Dates (Intl)
+// 📎 MDN intl
+
+// <180. Internationalizing Numbers (Intl)
+const num = 3884764.23;
+
+const options = {
+  // style: 'unit',
+  // style: 'percent',
+  style: 'currency',
+  // unit: 'mile-per-hour',
+  unit: 'celsius',
+  currency: 'EUR', // ⚠️ set the currency; it's not determined by the locale
+  // useGrouping: false,
+  // 📎 MDN
+};
+
+console.log('US:      ', new Intl.NumberFormat('en-US', options).format(num)); // US:       3,884,764.23
+console.log('Germany: ', new Intl.NumberFormat('de-DE', options).format(num)); // Germany:  3.884.764,23
+console.log('Syria:   ', new Intl.NumberFormat('ar-SY', options).format(num)); // Syria:    ٣٬٨٨٤٬٧٦٤٫٢٣
+console.log(
+  navigator.language,
+  new Intl.NumberFormat(navigator.language, options).format(num)
+); // ko-KR 3,884,764.23
+
+//
