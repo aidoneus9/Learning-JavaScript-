11e9c027464c19e0766210f724b930fe794a5c27
aidoneus9 <155718337+aidoneus9@users.noreply.github.com>
2024-06-06 23:43:02 +0900

    1st commit 🚀

---
+++ [Section 13] Advanced DOM and Events/script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// <185. PROJECT: "Bankist" Website>
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault(); // ✍️ index.html(36): this is actually now a link and not a button and on the link, when we have this hash(#) here as the hyperlink, so as HRF, then that will make the page jump to the top; that's the default behavior.
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
